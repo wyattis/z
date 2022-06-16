d6e073b50308be32f1e264f77c4c4f898df628fa
wyattis <wyattisrael@gmail.com>
2022-06-16 15:47:00 -0400

    zcsv: add zcsv module for map based CSV reader

---
+++ zcsv/zcsv.go
@@ -0,0 +1,95 @@
+package zcsv
+
+import (
+	"encoding/csv"
+	"errors"
+	"fmt"
+	"io"
+)
+
+var (
+	ErrInvalidColumn = errors.New("no column with that name exists")
+	ErrNoData        = errors.New("no data exists for that column")
+)
+
+func NewReader(source io.Reader, headers []string) (reader *CsvReader) {
+	r := csv.NewReader(source)
+	// r.ReuseRecord = true
+	reader = &CsvReader{
+		Reader: r,
+	}
+	if headers != nil && len(headers) > 0 {
+		reader.headers = make(map[string]int)
+		for i := range headers {
+			reader.headers[headers[i]] = i
+		}
+	}
+	return reader
+}
+
+type CsvReader struct {
+	*csv.Reader
+	headers map[string]int
+}
+
+func (r *CsvReader) Headers() (cols []string) {
+	for col := range r.headers {
+		cols = append(cols, col)
+	}
+	return
+}
+
+func (r *CsvReader) ReadAll() (lines []Line, err error) {
+	for {
+		line, err := r.Read()
+		if err != nil {
+			if err == io.EOF {
+				err = nil
+			}
+			return lines, err
+		}
+		lines = append(lines, line)
+	}
+}
+
+func (r *CsvReader) Read() (line Line, err error) {
+	if r.headers == nil {
+		r.headers = make(map[string]int)
+		headers, err := r.Reader.Read()
+		if err != nil {
+			return line, err
+		}
+		for i := range headers {
+			r.headers[headers[i]] = i
+		}
+	}
+	row, err := r.Reader.Read()
+	if err != nil {
+		return
+	}
+	line.data = row
+	line.headers = &r.headers
+	line.errOnInvalid = r.FieldsPerRecord >= 0
+	return
+}
+
+type Line struct {
+	data         []string
+	errOnInvalid bool
+	headers      *map[string]int
+}
+
+func (l *Line) Get(key string) (val string, err error) {
+	i, ok := (*l.headers)[key]
+	if !ok {
+		fmt.Println(key)
+		err = ErrInvalidColumn
+		return
+	}
+	if i < len(l.data) {
+		val = l.data[i]
+	} else if l.errOnInvalid {
+		err = ErrNoData
+	}
+	return
+}
