03c1c9c5613254e1241d9508402bc494a0a35638
wyattis <wyattisrael@gmail.com>
2022-11-16 11:22:14 -0500

    add ztext module for automatic text decoding

---
+++ ztext/ztext.go
@@ -0,0 +1,91 @@
+package ztext
+
+import (
+	"bufio"
+	"bytes"
+	"fmt"
+	"io"
+
+	"github.com/gogs/chardet"
+	"github.com/wyattis/z/zslice/zstrings"
+	"golang.org/x/text/encoding/ianaindex"
+	"golang.org/x/text/transform"
+)
+
+func NewUTF8Reader(source io.Reader) *UTF8Reader {
+	return &UTF8Reader{
+		source: source,
+		reader: bufio.NewReader(source),
+	}
+}
+
+type UTF8Reader struct {
+	source         io.Reader
+	reader         *bufio.Reader
+	encResult      *chardet.Result
+	decodingReader io.Reader
+}
+
+var boms = map[string][]byte{
+	"UTF-8":    {0xef, 0xbb, 0xbf},
+	"UTF-16LE": {0xff, 0xfe},
+	"UTF-16BE": {0xfe, 0xff},
+	"UTF-32BE": {0x00, 0x00, 0xfe, 0xff},
+	"UTF-32LE": {0xff, 0xfe, 0x00, 0x00},
+}
+
+func (r *UTF8Reader) deleteBom(head []byte, charset string) {
+	bom, ok := boms[charset]
+	if !ok {
+		return
+	}
+	if bytes.Equal(bom, head[:len(bom)]) {
+		r.reader.Discard(len(bom))
+	}
+}
+
+func (r *UTF8Reader) init() (err error) {
+	if r.encResult != nil {
+		return
+	}
+	head, err := r.reader.Peek(2048)
+	if err == io.EOF {
+		err = nil
+	} else if err != nil {
+		return
+	}
+	r.encResult, err = chardet.NewTextDetector().DetectBest(head)
+	if err != nil {
+		return
+	}
+	r.deleteBom(head, r.encResult.Charset)
+
+	if zstrings.ContainsAny([]string{"UTF-8"}, r.encResult.Charset) {
+		r.decodingReader = r.reader
+		return
+	}
+
+	e, err := ianaindex.MIME.Encoding(r.encResult.Charset)
+	if err != nil {
+		return err
+	} else if e == nil {
+		return fmt.Errorf("unsupported charset: %s", r.encResult.Charset)
+	}
+	r.decodingReader = transform.NewReader(r.reader, e.NewDecoder())
+	return
+}
+
+func (r *UTF8Reader) Read(d []byte) (n int, err error) {
+	if err = r.init(); err != nil {
+		return
+	}
+	n, err = r.decodingReader.Read(d)
+	return
+}
+
+func (r *UTF8Reader) Close() (err error) {
+	if c, ok := r.source.(io.Closer); ok {
+		return c.Close()
+	}
+	return
+}
---
+++ ztext/ztext_test.go
@@ -0,0 +1,36 @@
+package ztext
+
+import (
+	"io"
+	"io/fs"
+	"os"
+	"path/filepath"
+	"testing"
+
+	"github.com/andreyvit/diff"
+)
+
+var truth = []byte("Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.")
+
+func TestUTF8Reader(t *testing.T) {
+	filepath.WalkDir("./test", func(path string, d fs.DirEntry, err error) (err2 error) {
+		if d.IsDir() {
+			return
+		}
+		f, err := os.Open(path)
+		if err != nil {
+			return
+		}
+		defer f.Close()
+		reader := NewUTF8Reader(f)
+		data, err := io.ReadAll(reader)
+		if err != nil {
+			return
+		}
+		if string(truth) != string(data) {
+			os.WriteFile("dump.txt", append([]byte(path+"\n"), append(truth, append([]byte("\n"), append(data, []byte(diff.CharacterDiff(string(truth), string(data)))...)...)...)...), os.ModePerm)
+			t.Errorf("%s\nExpected\n%s\nreceived\n%s\n", path, string(truth), string(data))
+		}
+		return
+	})
+}
