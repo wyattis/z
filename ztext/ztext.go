1c2cea399c962bd0e293b92d80fa67adf950912e
wyattis <wyattisrael@gmail.com>
2022-11-18 16:52:56 -0500

    ztext: allow specifying charset in UTF8Reader

--- ztext/ztext.go
+++ ztext/ztext.go
@@ -11,6 +11,22 @@ import (
 	"golang.org/x/text/transform"
 )
 
+var boms = map[string][]byte{
+	"UTF-8":    {0xef, 0xbb, 0xbf},
+	"UTF-16LE": {0xff, 0xfe},
+	"UTF-16BE": {0xfe, 0xff},
+	"UTF-32BE": {0x00, 0x00, 0xfe, 0xff},
+	"UTF-32LE": {0xff, 0xfe, 0x00, 0x00},
+}
+
+func NewUTF8ReaderCharset(source io.Reader, charset string) *UTF8Reader {
+	return &UTF8Reader{
+		source:  source,
+		charset: charset,
+		reader:  bufio.NewReader(source),
+	}
+}
+
 func NewUTF8Reader(source io.Reader) *UTF8Reader {
 	return &UTF8Reader{
 		source: source,
@@ -21,16 +37,8 @@ func NewUTF8Reader(source io.Reader) *UTF8Reader {
 type UTF8Reader struct {
 	source         io.Reader
 	reader         *bufio.Reader
-	encResult      *chardet.Result
+	charset        string
 	decodingReader io.Reader
-}
-
-var boms = map[string][]byte{
-	"UTF-8":    {0xef, 0xbb, 0xbf},
-	"UTF-16LE": {0xff, 0xfe},
-	"UTF-16BE": {0xfe, 0xff},
-	"UTF-32BE": {0x00, 0x00, 0xfe, 0xff},
-	"UTF-32LE": {0xff, 0xfe, 0x00, 0x00},
 }
 
 func (r *UTF8Reader) deleteBom(head []byte, charset string) {
@@ -44,31 +52,37 @@ func (r *UTF8Reader) deleteBom(head []byte, charset string) {
 }
 
 func (r *UTF8Reader) init() (err error) {
-	if r.encResult != nil {
+	if r.decodingReader != nil {
 		return
 	}
-	head, err := r.reader.Peek(2048)
-	if err == io.EOF {
-		err = nil
-	} else if err != nil {
-		return
-	}
-	r.encResult, err = chardet.NewTextDetector().DetectBest(head)
-	if err != nil {
-		return
-	}
-	r.deleteBom(head, r.encResult.Charset)
 
-	if r.encResult.Charset == "UTF-8" {
+	head, err := r.reader.Peek(2048)
+	// Try to autodetect the encoding
+	if r.charset == "" {
+		if err == io.EOF {
+			err = nil
+		} else if err != nil {
+			return err
+		}
+		encResult, err := chardet.NewTextDetector().DetectBest(head)
+		if err != nil {
+			return err
+		}
+		r.charset = encResult.Charset
+	}
+
+	r.deleteBom(head, r.charset)
+
+	if r.charset == "UTF-8" {
 		r.decodingReader = r.reader
 		return
 	}
 
-	e, err := ianaindex.MIME.Encoding(r.encResult.Charset)
+	e, err := ianaindex.MIME.Encoding(r.charset)
 	if err != nil {
 		return err
 	} else if e == nil {
-		return fmt.Errorf("unsupported charset: %s", r.encResult.Charset)
+		return fmt.Errorf("unsupported charset: %s", r.charset)
 	}
 	r.decodingReader = transform.NewReader(r.reader, e.NewDecoder())
 	return
